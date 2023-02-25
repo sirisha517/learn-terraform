22368834a8353ea9c87f2e1cac1cdad2ba6a42cf
sirisha517 <sai.sirifriends8790@gmail.com>
2023-02-25 16:26:17 +0530

    updating learn-terraform

--- 03-data/data.tf
+++ 03-data/data.tf
@@ -1,7 +1,7 @@
 data "aws_ami" "ami" {
   most_recent      = true
   name_regex       = "Centos-8-DevOps-Practice" //AMI NAME
-  owners           = ["972179603668"] //OWNER
+  owners           = ["973714476881"] //OWNER
 }
 
 output "ami_id" {
