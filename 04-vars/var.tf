f8dfa7bf723cbdc5f1d90d5a4103c69a57f486a4
sirisha517 <sai.sirifriends8790@gmail.com>
2023-02-25 17:28:26 +0530

    updating learn-terraform

--- 04-vars/var.tf
+++ 04-vars/var.tf
@@ -13,3 +13,22 @@ output "sample1" {
 }
 
 variable "inputs" {}
+
+variable "sample5" {
+  default = "hello"
+}
+variable "sample6" {
+  default = [ "Hello", 1000, true, "world" ]
+}
+
+variable "sample7" {
+  default = {
+    string = "hello"
+    number = 53
+    boolean = true
+  }
+}
+
+output "types" {
+  value = "variable sample5 - ${var.sample5} , First values is - ${var.sample6[1]}, bool"
+}
