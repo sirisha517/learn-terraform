d1f2823dbd6b91815923a88d48aa7775f734750e
sirisha517 <sai.sirifriends8790@gmail.com>
2023-02-26 20:03:05 +0530

    updating learn-terraform

--- 04-vars/var.tf
+++ 04-vars/var.tf
@@ -30,5 +30,5 @@ variable "sample7" {
 }
 
 output "types" {
-  value = "variable sample5 - ${var.sample5} , First values in list - ${var.sample6[0]}, Boolean value of Map  = ${var.sample7[boolean]}"
+  value = "variable sample5 - ${var.sample5} , First values in list - ${var.sample6[0]}, Boolean value of Map  = ${var.sample7["boolean"]}"
 }
