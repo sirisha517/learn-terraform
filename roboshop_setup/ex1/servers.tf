d643d9e841aeed3d2db068956eb36c1f458f3cb1
sirisha517 <sai.sirifriends8790@gmail.com>
2023-02-27 15:19:17 +0530

    updating learn-terraform

--- roboshop_setup/ex1/servers.tf
+++ roboshop_setup/ex1/servers.tf
@@ -1,10 +1,11 @@
 data "aws_ami" "ami" {
- most_recent = true
-  name_regex = "Centos-8-DevOps-Practice"
-  owners = ["972179603668"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice" //AMI NAME
+  owners           = ["973714476881"] //OWNER
 }
+
 resource "aws_instance" "frontend" {
-  ami = "ami-0a017d8ceb274537d"
+  ami = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0cd76acf87f0514bd"]
   tags = {
@@ -152,4 +153,8 @@ resource "aws_route53_record" "Payment" {
   type    = "A"
   ttl     = 30
   records = [aws_instance.Payment.private_ip]
+}
+
+output "Payment" {
+  value = "public Ip address : ${aws_instance.Payment.public_ip}"
 }
--- roboshop_setup/ex2/main.tf
+++ roboshop_setup/ex2/main.tf
@@ -1,11 +1,12 @@
 data "aws_ami" "ami" {
-  most_recent = true
-  name_regex = "Centos-8-DevOps-Practice"
-  owners = ["972179603668"]
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice" //AMI NAME
+  owners           = ["973714476881"] //OWNER
 }
+
 resource "aws_instance" "frontend" {
   count = 5
-  ami = "ami-0a017d8ceb274537d"
+  ami = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
   vpc_security_group_ids = ["sg-0cd76acf87f0514bd"]
 }
