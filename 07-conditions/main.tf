dd6e412bc8cc09c74cf3b9ef93364cef27edf121
sirisha517 <sai.sirifriends8790@gmail.com>
2023-02-27 16:46:31 +0530

    updating learn-terraform

--- 07-conditions/main.tf
+++ 07-conditions/main.tf
@@ -8,7 +8,7 @@ variable "instance_type" {}
 variable "create_instance" {}
 
 
-resource "aws_instance" "test" {
+resource "aws_instance" "test1" {
   count = var.create_instance == "true" ? 1 : 0
   ami = "ami-0a017d8ceb274537d"
   instance_type = var.instance_type == "" ? "t3.micro" : var.instance_type
