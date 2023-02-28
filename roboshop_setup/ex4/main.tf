d7b070cc687793668ff22571c6c30b2b7930084a
sirisha517 <sai.sirifriends8790@gmail.com>
2023-02-28 14:29:58 +0530

    updating learn-terraform

--- roboshop_setup/ex4/main.tf
+++ roboshop_setup/ex4/main.tf
@@ -4,7 +4,9 @@ module "ec2" {
   component = each.value["name"]
   instance_type = each.value["type"]
 }
-
+module "sg" {
+  source = "./sg"
+}
 variable "instances" {
   default = {
     catalogue = {
