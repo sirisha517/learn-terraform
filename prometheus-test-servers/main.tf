b107370cae14a5c72175df3098d11457fec2a4cd
sirisha517 <sai.sirifriends8790@gmail.com>
2023-03-09 17:04:24 +0530

    prometheus server creation

--- prometheus-test-servers/main.tf
+++ prometheus-test-servers/main.tf
@@ -8,13 +8,24 @@ resource "aws_spot_instance_request" "node1" {
     Name = "promo-test-server"
   }
 }
+resource "aws_ec2_tag" "node1" {
+  resource_id = aws_spot_instance_request.node1.spot_instance_id
+  key         = "Name"
+  value       = "promo-test-server"
+}
+
 
 # Request a spot instance
 resource "aws_spot_instance_request" "node2" {
-  ami           = "ami-072a415581f03e0ce"
-  instance_type = "t3.micro"
+  ami                    = "ami-072a415581f03e0ce"
+  instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0cd76acf87f0514bd"]
-  tags = {
+  tags                   = {
     Name = "promo-test-node"
   }
 }
+  resource "aws_ec2_tag" "node2" {
+    resource_id = aws_spot_instance_request.node2.spot_instance_id
+    key         = "Name"
+    value       = "promo-test-node"
+  }
